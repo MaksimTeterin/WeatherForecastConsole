a8bc0c2a3468fa9c3e84d66489f205a7be3ec878
MaksimTeterin <maksim.padriku@gmail.com>
2024-02-01 20:24:34 +0200

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text.Json;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+using Newtonsoft.Json.Linq;
+
+
+namespace WeatherForecastConsole
+{
+    internal class Program
+    {
+        static async Task Main()
+    {
+        const string APIKEY = "https://api.met.no/weatherapi/locationforecast/2.0/compact?lat=59.436962&lon=24.753574";
+            HttpClient httpClient = new HttpClient();
+            httpClient.DefaultRequestHeaders.Accept.Clear();
+            httpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+            httpClient.DefaultRequestHeaders.Add("User-Agent", ".NET HttpClient");
+
+            HttpResponseMessage response = await httpClient.GetAsync(APIKEY);
+                response.EnsureSuccessStatusCode();
+            
+            string jsonResult = await response.Content.ReadAsStringAsync();
+            dynamic jsonData = JObject.Parse(jsonResult);
+                    for(int i = 0; i<jsonData.properties.timeseries.Count; i++)
+            {
+                Console.Write(i+1 + ".    ");
+                    Console.Write(jsonData.properties.timeseries[i].data.instant.details.air_temperature + " °C     ");
+                    Console.WriteLine(jsonData.properties.timeseries[i].time);
+                
+            }
+                    
+                
+            
+    }
+    }
+}
